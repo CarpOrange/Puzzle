3c02b51949a11b08bf25b39b551017fd262f68a8
LiYuchen <987424501@qq.com>
2022-05-20 17:55:19 +0800

    second commit

--- app/src/main/java/com/orangeisland/puzzle/MainActivity.kt
+++ app/src/main/java/com/orangeisland/puzzle/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.orangeisland.puzzle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.toBitmap
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.orangeisland.puzzle.ui.adapter.ImageRvAdapter
 import com.orangeisland.puzzle.ui.bean.ImageData
 import com.orangeisland.puzzle.util.ImageSplitter
+import com.yanzhenjie.recyclerview.SwipeRecyclerView
+import com.yanzhenjie.recyclerview.touch.OnItemMoveListener
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val rv: RecyclerView by lazy { findViewById<RecyclerView>(R.id.rv) }
+    private val rv: SwipeRecyclerView by lazy { findViewById<SwipeRecyclerView>(R.id.rv) }
     val adapter = ImageRvAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +30,24 @@ class MainActivity : AppCompatActivity() {
         val bitmap = resources.getDrawable(R.drawable.gintama).toBitmap()
         val imageDatas = ImageSplitter.split(bitmap, 3, 3).map{}.shuffled()
         adapter.setNewInstance(imageDatas)
+        rv.isLongPressDragEnabled = true
+        rv.setOnItemMoveListener(object : OnItemMoveListener {
+            override fun onItemMove(
+                srcHolder: RecyclerView.ViewHolder,
+                targetHolder: RecyclerView.ViewHolder
+            ): Boolean {
+                val fromPosition = srcHolder.adapterPosition
+                val toPosition = targetHolder.adapterPosition
+                Collections.swap(adapter.data, fromPosition, toPosition)
+                adapter.notifyItemMoved(fromPosition, toPosition)
+                checkSort()
+                return true
+            }
 
+            override fun onItemDismiss(srcHolder: RecyclerView.ViewHolder) {
+            }
 
+        })
     }
 
     fun checkSort() {
@@ -38,6 +58,9 @@ class MainActivity : AppCompatActivity() {
             if(index != imageData.index) {
                 isRightSort = false
             }
+        }
+        if(isRightSort) {
+            Toast.makeText(this, "bingo", Toast.LENGTH_LONG).show()
         }
 
     }
